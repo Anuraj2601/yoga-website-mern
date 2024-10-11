a403fe60984575a8236162ef0d3203c82292bd46
anuraj <anurajselvasothy@gmail.com>
2024-10-11 19:56:06 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000; 
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
+//MongoDB connection
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@yoga.delab.mongodb.net/?retryWrites=true&w=majority&appName=yoga`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // create a database and collections
+    const database = client.db("yoga");
+    const userCollection = database.collection("users");
+    const classCollection = database.collection("classes");
+    const cartCollection = database.collection("cart");
+    const paymentCollection = database.collection("payments");
+    const enrolledCollection = database.collection("enrolled");
+    const appliedCollection = database.collection("applied");
+
+    // classes routes
+    app.post('/new-class', async (req, res) => {
+        const newClass = req.body;
+        //newClass.availableSeats = parseInt(newClass.availableSeats);
+        const result = await classCollection.insertOne(newClass);
+        res.send(result);
+    })
+
+    app.get('/classes', async (req, res) => {
+        const query = { status: 'approved'};
+        const result = await classCollection.find().toArray();
+        res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch(err) {
+   console.error('Error connecting to MongoDB:',err);
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
