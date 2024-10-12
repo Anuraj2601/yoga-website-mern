55c23d29b5a0d0110eeac99675d644b4d420842d
anuraj <anurajselvasothy@gmail.com>
2024-10-12 18:22:42 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -36,70 +36,109 @@ async function run() {
     const enrolledCollection = database.collection("enrolled");
     const appliedCollection = database.collection("applied");
 
-    // classes routes
+    // classes Routes ---
 
-    // add new class
-    app.post('/new-class', async (req, res) => {
-        const newClass = req.body;
-        //newClass.availableSeats = parseInt(newClass.availableSeats);
-        const result = await classCollection.insertOne(newClass);
-        res.send(result);
-    })
+        // add new class
+        app.post('/new-class', async (req, res) => {
+            const newClass = req.body;
+            //newClass.availableSeats = parseInt(newClass.availableSeats);
+            const result = await classCollection.insertOne(newClass);
+            res.send(result);
+        })
 
-    // get all approved classes
-    app.get('/classes', async (req, res) => {
-        const query = { status: 'approved'};
-        const result = await classCollection.find().toArray();
-        res.send(result);
-    })
+        // get all approved classes
+        app.get('/classes', async (req, res) => {
+            const query = { status: 'approved'};
+            const result = await classCollection.find().toArray();
+            res.send(result);
+        })
 
-    // get classes by Instrutor email address
-    app.get('/classes/:email', async(req,res) => {
-        const email = req.params.email;
-        const query = {instructorEmail:  email};
-        const result = await classCollection.find(query).toArray();
-        res.send(result);
-    })
+        // get classes by Instrutor email address
+        app.get('/classes/:email', async(req,res) => {
+            const email = req.params.email;
+            const query = {instructorEmail:  email};
+            const result = await classCollection.find(query).toArray();
+            res.send(result);
+        })
 
-    // get all classes
-    app.get('/classes-manage', async(req,res) => {
-      const result = await classCollection.find().toArray();
-      res.send(result);
-    })
+        // get all classes
+        app.get('/classes-manage', async(req,res) => {
+          const result = await classCollection.find().toArray();
+          res.send(result);
+        })
 
-    // update classes status and reason
-    app.patch('/change-status/:id', async(req,res) => {
-        const id = req.params.id;
-        const status = req.body.status;
-        const reason = req.body.reason;
-        const filter = {_id: new ObjectId(id)}; // filter by class id
-        const options = { upsert: true};  // create a new document if any query doesn't match
-        const updateDoc = {
-          $set: {
-            status: status,
-            reason: reason
-          },
-        };
-        const result = await classCollection.updateOne(filter,updateDoc, options);
-        res.send(result);
-    })
+        // update classes status and reason
+        app.patch('/change-status/:id', async(req,res) => {
+            const id = req.params.id;
+            const status = req.body.status;
+            const reason = req.body.reason;
+            const filter = {_id: new ObjectId(id)}; // filter by class id
+            const options = { upsert: true};  // create a new document if any query doesn't match
+            const updateDoc = {
+              $set: {
+                status: status,
+                reason: reason
+              },
+            };
+            const result = await classCollection.updateOne(filter,updateDoc, options);
+            res.send(result);
+        })
 
-    // get approved classes
-    app.get('/approved-classes', async(req,res) => {
-      const query = {status: 'approved'};
-      const result = await classCollection.find(query).toArray();
-      res.send(result);
-    })
+        // get approved classes
+        app.get('/approved-classes', async(req,res) => {
+          const query = {status: 'approved'};
+          const result = await classCollection.find(query).toArray();
+          res.send(result);
+        })
 
-    // get single classe details
-     app.get('/class/:id', async(req,res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
-        const result = await classCollection.findOne(query);
-        res.send(result);
-     })
+        // get single classe details
+        app.get('/class/:id', async(req,res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await classCollection.findOne(query);
+            res.send(result);
+        })
 
+        // update class details
+        app.put('/update-class/:id', async(req,res) => {
+            const id = req.params.id;
+            const updateClass = req.body;
+            const filter = {_id: new ObjectId(id)};
+            const options = {upsert: true};
+            const updateDoc = {
+              $set : {
+                name: updateClass.name,
+                description: updateClass.description,
+                price: updateClass.price,
+                availableSeats: parseInt(updateClass.availableSeats),
+                videoLink: updateClass.videoLink,
+                status: 'pending',
+              },
+            };
+            const result = await classCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
 
+    // Card Routes  ---
+
+        // add to cart
+        app.post('/add-to-cart', async(req,res) => {
+            const newCartItem = req.body;
+            const result = await cartCollection.insertOne(newCartItem);
+            res.send(result);
+        })
+
+        // get Cart item by id
+        app.get('/cart-item/:id', async(req,res) => {
+            const id = req.params.id;
+            const email = req.body.email;
+            const query = {
+              classId: id,
+              userMail: email
+            };
+            projection: {classId: 1};
+            const result = await cartCollection.findOne(query, {projection: projection});
+        })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
